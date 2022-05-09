740b8128903bb211518c09290434164c3ce3e1ae
Rosemary Ku <rosemary.ku@hotmail.com>
2022-05-09 16:23:08 -0400

    add search logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [promptInput, setPromptInput] = useState("");
+  const [result, setResult] = useState();
+
+  const data = {
+    prompt: promptInput,
+    temperature: 0.5,
+    max_tokens: 64,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+  };
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const response = await fetch(
+      "https://api.openai.com/v1/engines/text-curie-001/completions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    const res = await response.json();
+    // console.log("Success:", res.choices[0].text);
+    setResult(res.choices[0].text);
+    setPromptInput("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fun with GPT-3</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="prompt"
+          placeholder="Enter a prompt"
+          value={promptInput}
+          onChange={(e) => setPromptInput(e.target.value)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+
+      <h3>Response:</h3>
+      <p>{result}</p>
     </div>
   );
 }
