ed22808aa418d75187f8673be32ab14d24544e18
Rosemary Ku <rosemary.ku@hotmail.com>
2022-05-10 10:13:00 -0400

    add select box for user to choose the AI engine

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,26 +1,24 @@
 import ls from "local-storage";
 import "./Search.scss";
+import { useState } from "react";
 
-const Search = ({
-  promptInput,
-  setPromptInput,
-  setResult,
-  responses,
-  setResponses,
-}) => {
+const Search = ({ promptInput, setPromptInput, responses, setResponses }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [engine, setEngine] = useState("text-curie-001");
+
   const data = {
     prompt: promptInput,
-    temperature: 0.5,
-    max_tokens: 64,
+    temperature: 0,
+    max_tokens: 60,
     top_p: 1.0,
     frequency_penalty: 0.0,
     presence_penalty: 0.0,
   };
 
-  async function handleSubmit(event) {
-    event.preventDefault();
+  async function handleSubmit() {
+    setIsLoading(true);
     const response = await fetch(
-      "https://api.openai.com/v1/engines/text-curie-001/completions",
+      `https://api.openai.com/v1/engines/${engine}/completions`,
       {
         method: "POST",
         headers: {
@@ -30,18 +28,20 @@ const Search = ({
         body: JSON.stringify(data),
       }
     );
-    const res = await response.json();
+    const answer = await response.json();
     let newResponses = [];
     if (responses == null) {
-      newResponses = [{ prompt: promptInput, response: res.choices[0].text }];
+      newResponses = [
+        { prompt: promptInput, response: answer.choices[0].text },
+      ];
     } else {
       newResponses = [
-        { prompt: promptInput, response: res.choices[0].text },
+        { prompt: promptInput, response: answer.choices[0].text },
         ...responses,
       ];
     }
-
     setResponses(newResponses);
+    setIsLoading(false);
     ls.set("myResponses", newResponses);
     setPromptInput("");
   }
@@ -55,24 +55,34 @@ const Search = ({
           human writing.
         </h4>
       </div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="prompt">
-          <b>Enter a prompt below</b>
-        </label>
-        <textarea
-          id="prompt"
-          name="prompt"
-          type="text"
-          rows="5"
-          cols="50"
-          value={promptInput}
-          onChange={(e) => setPromptInput(e.target.value)}
-        />
-        <br />
-        <button type="submit" value="Submit">
-          Submit
-        </button>
-      </form>
+
+      <label htmlFor="prompt">
+        <b>Ask a geography trivia question here:</b>
+      </label>
+      <textarea
+        id="prompt"
+        name="prompt"
+        type="text"
+        rows="5"
+        cols="50"
+        value={promptInput}
+        onChange={(e) => setPromptInput(e.target.value)}
+      />
+      <br />
+      <select
+        value={engine}
+        aria-label="Default select example"
+        onChange={(e) => setEngine(e.target.value)}
+      >
+        <option value="text-curie-001">Curie</option>
+        <option value="text-ada-001">Ada</option>
+        <option value="text-babbage-001">Babbage</option>
+        <option value="text-davinci-002">Davinci</option>
+      </select>
+
+      <button type="submit" onClick={handleSubmit}>
+        {isLoading ? "Loading ..." : "Submit"}
+      </button>
     </div>
   );
 };
